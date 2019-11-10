d55e375376b4aab6781f0fb69612ad9f52096d7b
Lucas Alexander <lucasalexandersb@gmail.com>
2019-11-10 20:35:24 -0300

    models de estudante e usuario, conexão com o banco

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+import Student from './app/models/Student';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Funfa' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Alexander',
+    email: 'lucasemail@email.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
